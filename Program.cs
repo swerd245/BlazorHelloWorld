a7aeed521a32ac02aa1f2e56d44fb51e3618fbc1
swerd245 <swerd245@kakao.com>
2024-02-23 19:21:50 +0900

    merge

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using BlazorHelloWorld.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,32 +25,3 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
-=======
-using BlazorHelloWorld.Components;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
->>>>>>> develop
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# BlazorHelloWorld
-Blazor 프로젝트를 생성 후 Azure DevOps를 거쳐 Azure Web App까지 게시
+push test
